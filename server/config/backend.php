78822dbed4a55f5e37130f75020db01d2c65762c
Melvin Olsthoorn <aaapanda0505@gmail.com>
2026-01-20 15:31:36 +0100

    Old files
    old files should not be pushed

--- server/config/backend.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array (
-  'status' => 'finalized',
-  'initialized' => true,
-  'locked' => true,
-  'admin_password_hash' => '$2y$10$7rXJI5SnVQqOWvtLehcWmewv7ksmqjG/hlU3CwKa.y8cUlvW/fl2a',
-);
